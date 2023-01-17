8fce0651ee94402a770570c2828a557bc606ef85
real-cisca <52496314+real-cisca@users.noreply.github.com>
2023-01-17 14:16:15 +0100

    Create brainteaser4.py
    The 4thnbrain teaser

---
+++ brainteaser4.py
@@ -0,0 +1,67 @@
+print(school)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    print(school)
+NameError: name 'school' is not defined
+student ID = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9}
+SyntaxError: invalid syntax
+studentID = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9}
+print('j':10)
+SyntaxError: invalid syntax
+print(j['10'])
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    print(j['10'])
+NameError: name 'j' is not defined
+open("student_file.txt","w")
+<_io.TextIOWrapper name='student_file.txt' mode='w' encoding='UTF-8'>
+open("student_file.txt","r")
+<_io.TextIOWrapper name='student_file.txt' mode='r' encoding='UTF-8'>
+print(studentID[11])
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print(studentID[11])
+KeyError: 11
+open("file.txt","r")
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    open("file.txt","r")
+FileNotFoundError: [Errno 2] No such file or directory: 'file.txt'
+open("studentfile.txt","w")
+<_io.TextIOWrapper name='studentfile.txt' mode='w' encoding='UTF-8'>
+open("studentfile.txt","r")
+<_io.TextIOWrapper name='studentfile.txt' mode='r' encoding='UTF-8'>
+print(studentID[12])
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    print(studentID[12])
+KeyError: 12
+studentID = ['a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9]
+SyntaxError: invalid syntax
+studentID = ['a': 1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9]
+SyntaxError: invalid syntax
+studentID = {'a':1, 'b':2, 'c':3, 'd':4, 'e':5, 'f':6, 'g':7, 'h':8, 'i':9}
+print
+<built-in function print>
+print(studentID{12})
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+open("student.txt". "r")
+SyntaxError: invalid syntax
+open("student.txt", "r")
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    open("student.txt", "r")
+FileNotFoundError: [Errno 2] No such file or directory: 'student.txt'
+open("studentfile.txt", "r")
+<_io.TextIOWrapper name='studentfile.txt' mode='r' encoding='UTF-8'>
+class_id = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(class_id["10"])
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    print(class_id["10"])
+TypeError: list indices must be integers or slices, not str
+print(class_id[10])
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    print(class_id[10])
+IndexError: list index out of range
