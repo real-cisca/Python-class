7a843395dfe21d4056411c7f4b89388ce23b13ea
real-cisca <52496314+real-cisca@users.noreply.github.com>
2023-02-07 12:37:27 +0100

    Create brainteaser8.py

---
+++ brainteaser8.py
@@ -0,0 +1,10 @@
+x = 5
+if x >= 5:
+    print("Congratulations you lucky bastard!")
+elif x >= 5:
+    print("Oops you've run out of luck!")
+else:
+    print("402 error")
+
+    
+Congratulations you lucky bastard!
